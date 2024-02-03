3ab766eca150cf6b6e3e50128b860f3b6b4b18c4
loginer1 <https://github.com/loginer1>
2024-02-03 19:12:29 +0200

    крч знову ізправив ошибки

---
+++ Assets/ScriptsForUI/SizeObserverUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SizeObserverUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform _observedElement;
+    [SerializeField] private Vector2 _ofset;
+
+    private RectTransform _myRectTransform;  
+
+    void Start()
+    {
+        _myRectTransform = GetComponent<RectTransform>();
+
+        if (_observedElement == null)
+        {
+            Debug.LogError("Не вказано елемент для спостереження!");
+            return;
+        }
+
+        _myRectTransform.sizeDelta = _observedElement.sizeDelta + _ofset;
+    }
+
+    void Update()
+    {
+        if (_observedElement.sizeDelta != _myRectTransform.sizeDelta)
+        {
+            _myRectTransform.sizeDelta = _observedElement.sizeDelta + _ofset;
+        }
+    }
+}
